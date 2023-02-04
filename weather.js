212e3bf0f44c5dbe737e518084305cb9bd2367bd
RynDaniel <ryan@clarusway.com>
2023-02-04 13:02:42 +0300

    secon lesson commit

--- weather.js
+++ weather.js
@@ -12,6 +12,9 @@ console.log(formJS);
 const formJQ =  $("form").eq(0);
 console.log(formJQ);
 const inputJQ = $("input");
+const msgJQ = $(".msg");
+const listJQ = $(".ajax-section .cities");
+
 // console.log($(formJS));
 
 //JS load event
@@ -26,8 +29,42 @@ const inputJQ = $("input");
 //JQuery DOMContentloaded
 // $(document).on("DOMContentLoaded", ()=>{console.log("jquery dom is ready!")});
 $(document).ready(()=>{
-    console.log("jquery dom is ready!")
+    console.log("jquery dom is ready!");
+    // localStorage.setItem("apiKey", EncryptStringAES("4d8fb5b93d4af21d66a2948710284366"));
 });
 
+//formJQ.on("submit", ()=>{});
+formJQ.submit((e)=>{
+    e.preventDefault();
+    getWeatherDataFromApi();
+});
 
+const getWeatherDataFromApi = async() =>{
+    const apiKey = DecryptStringAES(localStorage.getItem("apiKey"));
+    const cityNameInput = inputJQ.val();
+    const units = "metric";
+    const lang = "tr";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    //jquery http request
+    //const response = await $.get(url);
+    //const response = await $.post(url);
+    await $.ajax({
+            type : "GET",
+            url : url,
+            dataType : "json",
+            success: (response) => {
+                console.log(response);
+                //weather card
+            },
+            beforeSend : (request) => {
+                //parameter => headers
+                //send tokenKey
+            },
+            complete : ()=>{},
+            error : (XMLHttpRequest) =>{
+                //error handling
+            }
+        });
+    console.log(response);
+}
 
